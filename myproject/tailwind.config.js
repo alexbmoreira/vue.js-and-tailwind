c108fa841a4b924680e4b00f1dafd611e2b8cd9e
alexbmoreira <alexbmoreira@gmail.com>
2020-12-27 23:39:46 -0500

    Configure Tailwind to purge unused styles in production

--- myproject/tailwind.config.js
+++ myproject/tailwind.config.js
@@ -1,11 +1,12 @@
+// tailwind.config.js
 module.exports = {
-  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+      extend: {},
   },
   variants: {
-    extend: {},
+      extend: {},
   },
   plugins: [],
 }
